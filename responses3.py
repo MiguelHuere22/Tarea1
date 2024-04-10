89ec5d8fda76ec2d9d8e74a1149efa1465b592d3
MiguelHuere <miguel.huere@unmsm.edu.pe>
2024-04-10 18:45:16 -0500

    Primer Commit

---
+++ responses3.py
@@ -0,0 +1,9 @@
+from flask import Flask, redirect
+app = Flask(__name__)
+@app.route('/')
+
+def index():
+    return redirect('http://www.example.com')
+
+if __name__ == '__main__':
+    app.run(debug=True)
